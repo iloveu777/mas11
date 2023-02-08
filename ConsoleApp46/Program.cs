8b627599e4a5ff62256b841011255252878656d5
iloveu777 <116212823+iloveu777@users.noreply.github.com>
2023-02-08 14:14:49 +0200

    2 задача
    через 1 цилк

--- ConsoleApp46/Program.cs
+++ ConsoleApp46/Program.cs
@@ -11,27 +11,37 @@ namespace ConsoleApp46
     {
         static void Main(string[] args)
         {
-            Write("n=");
-            byte n = Convert.ToByte(Console.ReadLine());
-            int[] array = new int[n];
-            Random rnd = new Random();
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = rnd.Next(-20, 20);
-                WriteLine($"array[{i}] = {array[i]}");
-            }
-            byte count = 0;
-            /* for (int i = 0; i<array.Length; i++)
+            /* Write("n=");
+             byte n = Convert.ToByte(Console.ReadLine());
+             int[] array = new int[n];
+             Random rnd = new Random();
+             for (int i = 0; i < array.Length; i++)
              {
-                 if (array[i] < 0) count++;
+                 array[i] = rnd.Next(-20, 20);
+                 WriteLine($"array[{i}] = {array[i]}");
              }
-             WriteLine($"кол-во отриц. элементов ={count}");*/
-            foreach (int num in array)
+             byte count = 0;
+             /* for (int i = 0; i<array.Length; i++)
+              {
+                  if (array[i] < 0) count++;
+              }
+              WriteLine($"кол-во отриц. элементов ={count}");*/
+            /*foreach (int num in array)
             {
                 if (num < 0) count++;
 
             }
-            WriteLine($"кол-во отриц чисел = {count}");
+            WriteLine($"кол-во отриц чисел = {count}"); */
+            //задача 2 увеличить значение всех элементов в 2 раза
+
+            
+           int[] array = {-5, 5, 6, -6, -7, 7};
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i]*=2;
+                Write($"{array[i]}\t");
+            }
+
             ReadKey();
         }
         
